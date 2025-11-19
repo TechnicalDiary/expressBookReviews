c116db0e809d9bd077bb7011ef80f398a03c58be
TechnicalDiary <shahbazsharif092@gmail.com>
2025-11-19 12:36:52 +0530

    Change response status from 300 to 200 for ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,7 +31,7 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn  = req.params.isbn;
-    return res.status(300).json(books[isbn]);
+    return res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
